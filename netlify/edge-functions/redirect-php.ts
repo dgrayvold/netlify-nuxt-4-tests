6c3740e5f5dc9751da08167eb50502619b58f774
Daniel Grayvold <85690571+dgrayvold@users.noreply.github.com>
2025-04-09 04:03:25 -0500

    Log path for edge function runs

--- netlify/edge-functions/redirect-php.ts
+++ netlify/edge-functions/redirect-php.ts
@@ -1,7 +1,8 @@
 import type { Config, Context } from "@netlify/edge-functions";
 
 export default (req: Request, ctx: Context) => {
-    console.log('\nEdge function run\n');
+    console.log(`\nEdge function run on path: ${ctx.url.pathname}\n`);
+
     return new Response(null, {
         status: 404,
         headers: {
